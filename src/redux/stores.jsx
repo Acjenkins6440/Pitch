ba27aaf019e4c5f9a98927e883a97d97ff78e658
Erik Bean <erik.bean@nike.com>
2019-07-20 10:44:38 -0700

    wip deckReducer

--- src/redux/stores.jsx
+++ src/redux/stores.jsx
@@ -1,12 +1,13 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import actions_and_reducers from './actions_and_reducers.jsx'
+import deck from './deck';
 
 const initializeDeckState = () => {
   const deckState = {};
-  deckState["inPlay"] = {};
-  deckState["inDeck"] = {};
+  deckState.inPlay = {};
+  deckState.inDeck = {};
   for (var i = 1; i < 53; i++) {
-    deckState["inDeck"][i] = i;
+    deckState.inDeck[i] = i;
   }
   deckState["drawnCard"] = {};
   return deckState;
@@ -14,6 +15,9 @@ const initializeDeckState = () => {
 const deckState = initializeDeckState();
 
 const draw = actions_and_reducers['draw_action'];
+const reducer = combineReducers({
+  deck,
+})
 const deckStore = createStore(draw, deckState);
 const stores = {
   deckStore: deckStore
