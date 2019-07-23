9e6059d0cb2dc719957b898d4760c208fd1581b8
Alexander Jenkins <acjenkins6440@gmail.com>
2019-07-23 08:27:53 -0700

    Making deckState an array

--- src/redux/deck.js
+++ src/redux/deck.js
@@ -1,8 +1,47 @@
-const initialState = [];
+//TODO figure out how to initialize state inside reducer
+//Also figure out how to do drawCard() and resetAndShuffle() inside reducer?
+const initializeState = () => {
+  const deckArray = [];
+  for (var i = 1; i < 14; i++) {
+    deckArray.push(`h${i}`);
+    deckArray.push(`d${i}`);
+    deckArray.push(`s${i}`);
+    deckArray.push(`c${i}`);
+  }
+  return deckArray;
+};
+const initialState = initializeState();
 
-export default function deckReducer (state = initialState, action) {
-  switch(action.type){
+function resetAndShuffle() {
+  newDeck = initializeState();
+    for (var i = newDeck.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = newDeck[i];
+        newDeck[i] = newDeck[j];
+        newDeck[j] = temp;
+    }
+    return newDeck;
+}
+
+export default function deckReducer(state = initialState, action) {
+  switch (action.type) {
+    case shuffle:
+      return resetAndShuffle();
+    case draw:
+      return state.slice(0,1);
     default:
       return state;
+  }
+}
+
+const shuffleDeck = () => {
+  return{
+    type: "shuffle"
+  }
+}
+
+const drawCard = () => {
+  return{
+    type: "draw"
   }
 }
