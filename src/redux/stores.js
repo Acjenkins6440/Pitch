6f80b5bbb2e2d2a73d082fa1969d97154a675b04
Alexander Jenkins <acjenkins6440@gmail.com>
2019-07-23 08:27:53 -0700

    Making deckState an array

--- src/redux/stores.js
+++ src/redux/stores.js
@@ -3,30 +3,18 @@ import { createStore, combineReducers } from 'redux'
 import actions_and_reducers from './actions_and_reducers'
 import deck from './deck';
 
-const initializeDeckState = () => {
-  const deckState = {};
-  deckState.inPlay = {};
-  deckState.inDeck = {};
-  for (var i = 1; i < 53; i++) {
-    deckState.inDeck[i] = i;
-  }
-  deckState["drawnCard"] = {};
-  return deckState;
-};
-// TODO: Let each sub-reducer handle its own initialState
-const deckState = initializeDeckState();
-
-// TODO: get rid of actions_and_reducers object, use separate files 
-// with their own exported actionCreators and reducers  under /redux folder 
+// TODO: get rid of actions_and_reducers object, use separate files
+// with their own exported actionCreators and reducers  under /redux folder
 const draw = actions_and_reducers['draw_action'];
 
 // TODO: use this reducer -EB 07/20/19
 const reducer = combineReducers({
   deck,
 })
-const deckStore = createStore(draw, deckState);
+const deckStore = createStore(deck);
+console.log(deckStore.getState())
 
-// TODO: just have 1 store - its the redux thing to do 
+// TODO: just have 1 store - its the redux thing to do
 const stores = {
   deckStore: deckStore
 };
