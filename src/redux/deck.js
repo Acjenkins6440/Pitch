9ae8559e530deefc52856605671ce35a206a8686
Alexander Jenkins <acjenkins6440@gmail.com>
2019-07-24 19:44:39 -0700

    Refactored many files
    - Changed deck state from object to array
    - Renamed and removed files
    - Implemented Redux Devtools

--- src/redux/deck.js
+++ src/redux/deck.js
@@ -1,8 +1,8 @@
-//TODO figure out how to initialize state inside reducer
-//Also figure out how to do drawCard() and resetAndShuffle() inside reducer?
+// TODO figure out how to initialize state inside reducer
+// Also figure out how to do drawCard() and resetAndShuffle() inside reducer?
 const initializeState = () => {
   const deckArray = [];
-  for (var i = 1; i < 14; i++) {
+  for (let i = 1; i < 14; i++) {
     deckArray.push(`h${i}`);
     deckArray.push(`d${i}`);
     deckArray.push(`s${i}`);
@@ -13,35 +13,25 @@ const initializeState = () => {
 const initialState = initializeState();
 
 function resetAndShuffle() {
-  newDeck = initializeState();
-    for (var i = newDeck.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = newDeck[i];
-        newDeck[i] = newDeck[j];
-        newDeck[j] = temp;
-    }
-    return newDeck;
+  const newDeck = initializeState();
+  for (let i = newDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = newDeck[i];
+    newDeck[i] = newDeck[j];
+    newDeck[j] = temp;
+  }
+  return newDeck;
 }
 
 export default function deckReducer(state = initialState, action) {
   switch (action.type) {
-    case shuffle:
+    case 'shuffle':
       return resetAndShuffle();
-    case draw:
-      return state.slice(0,1);
     default:
       return state;
   }
 }
 
-const shuffleDeck = () => {
-  return{
-    type: "shuffle"
-  }
-}
-
-const drawCard = () => {
-  return{
-    type: "draw"
-  }
-}
+export const shuffleDeck = () => ({
+  type: 'shuffle',
+});
