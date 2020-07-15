531cdbb6f30afa47ede5bcd31da7027d1e594236
Alexander Jenkins <acjenkins6440@gmail.com>
2020-07-14 19:50:19 -0700

    Added card svg's

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import injectSheet from 'react-jss';
-import cardStyles from './cardStyles.jss';
 import cardBack from '../assets/cardback.jpeg'
 
 const numMap = {
@@ -10,61 +8,31 @@ const numMap = {
   12: 'Q',
   13: 'K',
 };
-const suitMap = {
-  d: '♦',
-  h: '♥',
-  c: '♣',
-  s: '♠',
-};
 
-const keyToNum = (cardKey) => {
-  const numKey = (cardKey.length === 3) ? cardKey.slice(-2) : cardKey.slice(-1);
-  return Object.keys(numMap).includes(numKey) ? numMap[numKey] : numKey;
-};
-
-const keyToSuit = cardKey => suitMap[cardKey[0]];
-
-const UnstyledCard = ({ cardKey, classes }) => {
-  const card = cardKey !== "blank" ? {
-    suit: keyToSuit(cardKey),
-    number: keyToNum(cardKey),
-  } : 
-  'blank';
-  if(card == 'blank'){
+const Card = ({ cardKey }) => {
+  if(cardKey == 'blank'){
     return (
-      <div className={classes.card}>
+      <div className="card">
         <div className="card-back">
-          <img src={cardBack} width="100%" height="100%" />
+          <img src={cardBack}/>
         </div>
       </div>
     );
   }
   else{
+    const imgSrc = `/public/images/${cardKey}.svg`
     return (
-      <div className={classes.card}>
-        <div className="card-front">
-          <div className={(card.suit === '♥' || card.suit === '♦') ? classes.redSuit : classes.blackSuit}>
-            <span className={classes.ulSymbol}>{card.suit}</span>
-            <span className={classes.number}>{card.number}</span>
-            <span className={classes.brSymbol}>{card.suit}</span>
-          </div>
+      <div className="card">
+        <div className="front">
+          <img src={imgSrc} />
         </div>
       </div>
     );
   }
 };
-const Card = injectSheet(cardStyles)(UnstyledCard);
 
-UnstyledCard.propTypes = {
+Card.propTypes = {
   cardKey: PropTypes.string.isRequired,
-  classes: PropTypes.shape({
-    blackSuit: PropTypes.string.isRequired,
-    brSymbol: PropTypes.string.isRequired,
-    card: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-    redSuit: PropTypes.string.isRequired,
-    ulSymbol: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
 export default Card;
--- src/components/Hand.js
+++ src/components/Hand.js
@@ -5,7 +5,7 @@ import Card from './Card';
 
 
 const Hand = ({ playerNum, playerSeat }) => {
-  const defaultState = (playerNum == playerSeat) ? ['h3', 'd13', 'c3', 'h11', 'd2', 'd9'] : ['blank', 'blank', 'blank', 'blank', 'blank', 'blank']
+  const defaultState = (playerNum == playerSeat) ? ['3H', 'KD', '3C', 'JH', '2D', '9D'] : ['blank', 'blank', 'blank', 'blank', 'blank', 'blank']
   const [playerHand, setPlayerHand] = useState(defaultState)
   const cards = []
   playerHand.forEach((card, index) => {
