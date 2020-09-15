fdde63290f621c59734e63b27e5e4a08ce71e7b7
Alexander Jenkins <acjenkins6440@gmail.com>
2020-09-15 14:04:56 -0700

    Fixed the linting error THANK GOODNESS

--- src/components/ScorePile.js
+++ src/components/ScorePile.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Container from 'react-bootstrap/container';
-import Row from 'react-bootstrap/row';
-import Col from 'react-bootstrap/col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Card from './Card';
 
 const ScorePile = ({ activeGame, mainPlayerIndex }) => {
